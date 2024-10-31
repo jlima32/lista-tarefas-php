421c5ba1f39a0e755fbf1b315b4c5d2785d06947
Juan Lima <juangustavo@gmail.com>
2024-10-31 11:20:48 -0300

    feat: listando as tarefas cadastradas na página inicial

---
+++ home.php
@@ -0,0 +1,43 @@
+
+<div id="tabela-tarefas">
+                <header>
+                    <div class="h-id">ID</div>
+                    <div class="h-ordem"></div>
+                    <div class="h-nome">Nome</div>
+                    <div class="h-custo">Custo</div>
+                    <div class="h-data-limite">Data Limite</div>
+                    <div class="h-data-acoes"></div>
+                </header>
+                <?php foreach($tarefas as $tarefa) :?>
+                    <div class="tarefa">
+                        <div class="tarefa-id"><?= $tarefa['id'] ?></div>
+                        <div class="tarefa-ordem">
+                            <span class="material-symbols-outlined icon">
+                                keyboard_arrow_up
+                            </span>
+                            <span class="material-symbols-outlined icon">
+                                keyboard_arrow_down
+                            </span>
+                        </div>
+                        <div class="tarefa-nome"><?= $tarefa['nome'] ?></div>
+                        <div class="tarefa-custo">R$ <?= $tarefa['custo'] ?></div>
+                        <div class="tarefa-data-limite"><?= $tarefa['data_limite'] ?></div>
+                        <div class="tarefa-acoes">
+                            <span class="material-symbols-outlined icon edit">
+                                edit_square
+                            </span>
+                            <span class="material-symbols-outlined icon delete">
+                                delete
+                            </span>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+                
+
+                <div class="add">
+                    <button class="btn-add">
+                        Adicionar Tarefa
+                    </button>
+                </div>
+
+            </div>
--- index.php
+++ index.php
@@ -2,8 +2,37 @@
 
 include_once 'DAO/TarefaDAO.php';
 
-$tarefas = new TarefaDAO();
+$tarefaDao = new TarefaDAO();
+$tarefas = $tarefaDao->listar();
+// echo "<pre>";
+// var_dump($tarefas->listar());
+// echo "</pre>";
 
-echo "<pre>";
-var_dump($tarefas->listar());
-echo "</pre>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Tarefas</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <main>
+        <div id="lista-tarefas">
+            <header>
+                <h1>Lista de Tarefas</h1>
+            </header>
+            
+            <?php include_once 'home.php'; ?>
+
+        </div>
+    </main>
+</body>
+</html>
