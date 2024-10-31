53a44ef75fecf86684fc89e7a12df2d49b25fccd
Juan Lima <juangustavo@gmail.com>
2024-10-31 20:22:06 -0300

    feat: passando os dados para o modal ao clicar em editar

--- home.php
+++ home.php
@@ -26,12 +26,14 @@
                             </button>
                         </div>
                         <div class="tarefa-nome"><?= $tarefa['nome'] ?></div>
-                        <div class="tarefa-custo">R$ <?= $tarefa['custo'] ?></div>
+                        <div class="tarefa-custo" >R$ <?= $tarefa['custo'] ?></div>
                         <div class="tarefa-data-limite"><?= $tarefa['data_limite'] ?></div>
                         <div class="tarefa-acoes">
-                            <span class="material-symbols-outlined icon edit">
-                                edit_square
-                            </span>
+                            <button class="btn-edit" data-nome="<?php echo $tarefa['nome']; ?>" data-custo="<?php echo $tarefa['custo']; ?>" data-limite="<?php echo $tarefa['data_limite']; ?>"data-id="<?php echo $tarefa['id']; ?>">
+                                <span class="material-symbols-outlined icon edit">
+                                    edit_square
+                                </span>
+                            </button>
                             <span class="material-symbols-outlined icon delete" onclick="excluir(<?= $tarefa['id'] ?>)">
                                 delete
                             </span>
@@ -61,13 +63,45 @@
                    </div>
             </dialog>
 
+            <dialog id="dialogEditar">
+                <div class="dialog-header">
+                    <div class="dialog-header-titulo">
+                        <h4>EDITAR TAREFA</h4>
+                    </div>
+                    <span class="material-symbols-outlined close" onclick="closeDialog()">
+                        disabled_by_default
+                    </span>
+                </div>
+
+                <div id="dialog-form">
+                    <form action="?pg=cadastrar&salvar=true" method="post">
+                    <div class="form-input">
+                        <label for="nome">Nome:</label>
+                        <input type="text" name="nome" id="nome">
+                    </div>
+                    <div class="form-input">
+                        <label for="custo">Custo:</label>
+                        <input type="number" name="custo" step="0.1" id="custo" placeholder="ex: 1000,00">
+                    </div>
+                    <div class="form-input">
+                        <label for="data_limite">Data Limite:</label>
+                        <input type="date" name="data_limite" id="data_limite">
+                    </div>
+                    <button type="submit" class="btn-add" >Editar</button>
+                    </form>
+                </div>
+
+            </dialog>
+
 
 <script>
-    const dialogExcluir = document.getElementById('dialogExcluir');
-    const dialogConteudo = document.getElementById('dialog-conteudo');
     
+    //Modal Excluir
     function excluir(id){
+        const dialogExcluir = document.getElementById('dialogExcluir');
+        const dialogConteudo = document.getElementById('dialog-conteudo');
         dialogExcluir.showModal();
+        
         dialogConteudo.innerHTML = `
         <p>Tem certeza que deseja remover a tarefa com<strong> id ${id} </strong>?</p>
         <div class='dialog-btn'>
@@ -76,9 +110,28 @@
         </div>
         `;
     }
+    
+    // Modal Editar
+        const dialogEditar = document.getElementById('dialogEditar');
+    
+        document.querySelectorAll('.btn-edit').forEach(button => {
+            button.addEventListener('click', () =>{
+                document.getElementById('nome').value = button.getAttribute('data-nome');
+                document.getElementById('custo').value = button.getAttribute('data-custo');
+                document.getElementById('data_limite').value = button.getAttribute('data-limite');
+                dialogEditar.showModal();
+            })
 
+        })
+        
+
+
+        
+
+    
     function closeDialog(){
         dialogExcluir.close();
+        dialogEditar.close();
     }
 
     //Reordenar tarefas
